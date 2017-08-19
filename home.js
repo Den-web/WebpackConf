8afb86b4a56f8ff546e221d0b2b71957bb2721f5
Den-web <kirevden@gmail.com>
2017-08-20 01:01:09 +0300

    First Light conf Webpack

---
+++ home.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Den on 20.08.2017.
+ */
+'use strict';
+
+let welcome = require('./welcome');
+
+welcome("home");
---
+++ welcome.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Den on 20.08.2017.
+ */
+'use strict';
+
+module.exports = function(message){
+    alert(`Welcome ${message}`);
+};
